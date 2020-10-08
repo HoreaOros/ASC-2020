f91a64ae655f38c1030aff7cf7cfcfa833c8e4de
Horea Oros <horea.oros@gmail.com>
2020-10-08 15:15:19 +0300

    create Moore project

---
+++ Moore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // rezolva problema
+
+        }
+    }
+}
